a2911755b25229d58acbdc010648917ecae93827
Noel Barrios <noel.barrios@optum.com>
2020-01-21 12:54:10 -0600

    initial setup

---
+++ src/Child/Child.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+// function Child(props) {
+//   return <h1>Hello, {props.name}</h1>;
+// }
+
+const Child = props => {
+  return <h1>Child</h1>;
+};
+
+export default Child;
