c304c0723e88ccf7443da021e5bb6e18ba3dd1ca
Noel Barrios <noel.barrios@optum.com>
2020-01-22 09:41:04 -0600

    add css stylehsheets; update state in the parent component from child

--- src/Child/Child.js
+++ src/Child/Child.js
@@ -1,11 +1,29 @@
 import React from "react";
+import "./Child.css";
 
-// function Child(props) {
-//   return <h1>Hello, {props.name}</h1>;
-// }
+class Child extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-const Child = props => {
-  return <p>Child</p>;
-};
+  buttonClicked = () => {
+    let randomNumber = Math.random() * 1;
+    if (randomNumber > 0.5) {
+      this.props.onNameChange("Mother");
+    } else {
+      this.props.onNameChange("Father");
+    }
+  };
+
+  render() {
+    return (
+      <div className="ButtonContainer">
+        <button className="myButton" onClick={this.buttonClicked}>
+          child
+        </button>
+      </div>
+    );
+  }
+}
 
 export default Child;
--- src/Parent/Parent.js
+++ src/Parent/Parent.js
@@ -1,13 +1,24 @@
 import React from "react";
 import Child from "./../Child/Child";
+import "./Parent.css";
 
 export default class Parent extends React.Component {
+  state = {
+    parentName: "Parent"
+  };
+  onChange = newName => {
+    this.setState({ parentName: newName });
+  };
+
   render() {
     return (
-      <div>
-        <h1>Parent</h1>
+      <div className="ParentContainer">
+        <h1>{this.state.parentName}</h1>
 
-        <Child></Child>
+        <Child
+          childName={this.state.parentName}
+          onNameChange={this.onChange}
+        ></Child>
       </div>
     );
   }
